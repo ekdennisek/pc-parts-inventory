fd8256ad752d82b07f0f2db079e63b925cb06382
Dennis Ek <dennis.a.ek@gmail.com>
2026-03-04 20:07:04 +0100

    Add 2nd gen Core i CPUs

--- frontend/src/data/cpus/intel.ts
+++ frontend/src/data/cpus/intel.ts
@@ -1,6 +1,7 @@
 import type { IntelSocket } from "../../types";
 import { core2 } from "./intel/core2";
 import { coreI1stGen } from "./intel/coreI1stGen";
+import { coreI2ndGen } from "./intel/coreI2ndGen";
 
 export type IntelCpu = {
   name: string;
@@ -12,4 +13,4 @@ export type IntelCpu = {
   note?: string;
 };
 
-export const intelCpus: IntelCpu[] = [...core2, ...coreI1stGen];
+export const intelCpus: IntelCpu[] = [...core2, ...coreI1stGen, ...coreI2ndGen];
---
+++ frontend/src/data/cpus/intel/coreI2ndGen.ts
@@ -0,0 +1,136 @@
+import type { IntelCpu } from "../intel";
+
+const sandyBridge: IntelCpu[] = [
+  {
+    name: "Core i7-2700K",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i7-2600K",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i7-2600",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i7-2600S",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2550K",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2500K",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2500",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2500S",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2500T",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2450P",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2400",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2405S",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2400S",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2390T",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2380P",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2320",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2310",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i5-2300",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2130",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2125",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2120",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2120T",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2105",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2102",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2100",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+  {
+    name: "Core i3-2100T",
+    socket: "LGA 1155",
+    codeName: "Sandy Bridge",
+  },
+];
+
+export const coreI2ndGen: IntelCpu[] = [...sandyBridge];
