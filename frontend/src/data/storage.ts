0a3a6964119bede5616d16afafffe8e45bcf37fc
Dennis Ek <dennis.a.ek@gmail.com>
2026-03-12 21:07:55 +0100

    Add initial storage devices

--- frontend/src/data/storage.ts
+++ frontend/src/data/storage.ts
@@ -1,3 +1,39 @@
 import type { Storage } from "../types";
 
-export const storage: Storage[] = [];
+export const storage: Storage[] = [
+  {
+    id: "storage-1",
+    name: "WD Green WD20EARX",
+    brand: "Western Digital",
+    description: "2TB SATA drive WD20EARX-008FB0",
+    releaseYear: 2012, // Production year
+    capacity: 2048,
+    type: "HDD",
+    formFactor: '3.5"',
+    interface: "SATA III",
+    rpm: 7200,
+  },
+  {
+    id: "storage-2",
+    name: "WD Green WD20EARX",
+    brand: "Western Digital",
+    description: "2TB SATA drive WD20EARX-008FB0",
+    releaseYear: 2012, // Production year
+    capacity: 2048,
+    type: "HDD",
+    formFactor: '3.5"',
+    interface: "SATA III",
+    rpm: 7200,
+  },
+  {
+    id: "storage-3",
+    name: "Lite-On CV8-8E128-11",
+    brand: "Lite-On",
+    description: "128GB SATA M.2",
+    releaseYear: 2019, // Production year
+    capacity: 128,
+    type: "SSD",
+    formFactor: "M.2",
+    interface: "SATA III",
+  },
+];
